58a3245d496a506ebf5c8b4d298ee11bf9638014
trachanh1609 <Vu@Users-MBP.home>
2016-11-30 19:32:08 +0200

    cors in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 
 // accept GET request
-router.get('/', function(req, res, next){
+router.get('/', cors(), function(req, res, next){
   res.render('index.html');
 });
 
