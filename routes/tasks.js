b4db1ee24bef1e5caea12d892c5c18f29e35eaef
trachanh1609 <Vu@Users-MBP.home>
2016-11-30 12:00:46 +0200

    fix database connection

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var mongojs = require('mongojs');
-var db = mongojs('mongodb://vunguyen:xWL-KsQ-3sQ-WeK@ds135577.mlab.com:35577/mytasklist_vu', ['tasks']);
+// var mongojs = require('mongojs');
+// var db = mongojs('mongodb://vunguyen:xWL-KsQ-3sQ-WeK@ds135577.mlab.com:35577/mytasklist_vu', ['tasks']);
 
+var databaseUrl = process.env.MONGODB_URI; // "username:password@example.com/mydb"
+var collections = ["tasks"];
+var db = require("mongojs").connect(databaseUrl, collections);
 // accept GET request
 // GET all tasks
 router.get('/tasks', function(req, res, next){
